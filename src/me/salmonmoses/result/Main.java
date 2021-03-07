d44eda8541bbfa25845dd8df57723b60dfe0d83f
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-03-07 22:03:44 +0200

    Added example with combined then/onError

--- src/me/salmonmoses/result/Main.java
+++ src/me/salmonmoses/result/Main.java
@@ -1,10 +1,16 @@
 package me.salmonmoses.result;
 
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
 		getString().map(s -> s + "!")
 		           .then(System.out::println);
-		var sad = getError().onError(e -> System.out.println(e.getMessage()));
+
+		getError().onError(e -> System.out.println(e.getMessage()));
+
+		getRandomError().then(System.out::println)
+		                .onError(e -> System.out.println(e.getMessage()));
 	}
 
 	private static Result<String, Exception> getString() {
@@ -13,5 +19,10 @@ public class Main {
 
 	private static Result<String, Exception> getError() {
 		return Result.error(new Exception("Saaaaaad"));
+	}
+
+	private static Result<String, Exception> getRandomError() {
+		Random r = new Random();
+		return r.nextBoolean() ? Result.ok("Value") : Result.error(new Exception("Error"));
 	}
 }
